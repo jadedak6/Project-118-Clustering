8530f6396b2d557ff9d8db078e2b8517dbbe6286
Jade Dak <68837640+jadedak6@users.noreply.github.com>
2021-06-07 13:35:23 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,36 @@
+from google.colab import files
+data_to_load = files.upload()
+
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("stars.csv")
+
+print(df.head())
+
+fig = px.scatter(df, x = "Size", y = "Light")
+fig.show()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize = (10,5))
+sns.lineplot(range(1, 11), wcss, marker='o', color='red')
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+sns.scatterplot(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], color = 'yellow', label = 'Cluster 1')
+sns.scatterplot(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], color = 'blue', label = 'Cluster 2')
+sns.scatterplot(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], color = 'green', label = 'Cluster 3')
+sns.scatterplot(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], color = 'red', label = 'Centroids',s=100,marker=',')
+
+plt.grid(False)
+plt.title('Stars')
+plt.xlabel('Size')
+plt.ylabel('Light')
+plt.legend()
+plt.show()
